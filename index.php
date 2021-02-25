cd3fa48c6528503a8c18145e982ae47a3f09addc
Adam Plumb <adamplumb@gmail.com>
2021-02-25 18:43:27 -0500

    Initial work in expanding weenie types

--- index.php
+++ index.php
@@ -5,7 +5,7 @@ include_once 'util.php';
 // Properties: https://github.com/ACEmulator/ACE/tree/master/Source/ACE.Entity/Enum/Properties
 
 $name = isset($_GET['name']) ? $_GET['name'] : '';
-$results = array();
+$resultsByType = array();
 
 if ($name) {
 
@@ -22,29 +22,37 @@ if ($name) {
                                     wps.value name,
                                     weenie.class_Name code,
                                     wpi.value level,
-                                    wpiType.value type
+                                    wpiType.value creatureType,
+                                    weenie.type type
                                 from weenie 
                                     join weenie_properties_string wps on (wps.object_Id = weenie.class_Id and wps.type = 1) 
                                     left join weenie_properties_bool wpb on (wpb.object_Id = weenie.class_Id and wpb.type = 19)
-                                    join weenie_properties_int wpi on (wpi.object_id = weenie.class_Id and wpi.type = 25)
-                                    join weenie_properties_int wpiType on (wpiType.object_Id = weenie.class_Id and wpiType.type = 2)
+                                    left join weenie_properties_int wpi on (wpi.object_id = weenie.class_Id and wpi.type = 25)
+                                    left join weenie_properties_int wpiType on (wpiType.object_Id = weenie.class_Id and wpiType.type = 2)
                                 where 
-                                    weenie.type in (10, 15)
-                                    and (wpb.value = 1 or wpb.value is null)
-                                    and (wps.value like ? or weenie.class_Name like ?)
-                                order by level asc, id asc");
+                                    (wps.value like ? or weenie.class_Name like ?)
+                                order by level asc, name asc");
 
     $statement->execute(array("%${name}%", "%${name}%"));
 
     while ($row = $statement->fetch()) {
-        $results[] = $row;
+        $type = $row['type'];
+        if (!isset($resultsByType[$type])) {
+            $resultsByType[$type] = array();
+        }
+
+        $resultsByType[$type][] = $row;
     }
     
-    if (count($results) == 1) {
-        header("Location: mob.php?id={$results[0]['id']}\n");
+    $keys = array_keys($resultsByType);
+    $numKeys = count($keys);
+    
+    if ($numKeys == 1 && count($resultsByType[$keys[0]]) == 1) {
+        header("Location: mob.php?id={$resultsByType[$keys[0]][0]['id']}\n");
         exit;
     }
 }
+
 ?>
 
 <html>
@@ -64,11 +72,14 @@ if ($name) {
     <input type="submit" value="Lookup" />
     </form>
 </div>
+<br />
 
 <?php
 if ($name) {
+    foreach ($resultsByType as $type => $results) {
+        if ($type == 10 || $type == 15) {
 ?>
-<br />
+<h3>Creature</h3>
 <table class="horizontal-table lookup-table" align="center">
 <thead>
     <tr>
@@ -81,24 +92,55 @@ if ($name) {
 </thead>
 <tbody>
 <?php
-    $index = 0;
-    foreach ($results as $row) {
-        $rowClass = $index % 2 == 1 ? ' class="alt"' : '';
+            $index = 0;
+            foreach ($results as $row) {
+                $rowClass = $index % 2 == 1 ? ' class="alt"' : '';
 ?>
     <tr<?php echo $rowClass; ?>>
         <td><?php echo $row['id']; ?></td>
         <td><a href="mob.php?id=<?php echo $row['id']; ?>"><?php echo $row['name']; ?></a></td>
-        <td><?php echo CREATURE_TYPE[$row['type']]; ?></td>
+        <td><?php echo CREATURE_TYPE[$row['creatureType']]; ?></td>
         <td><?php echo $row['code']; ?></td>
         <td><?php echo $row['level']; ?></td>
     </tr>
 <?php
-        $index++;
-    }
+                $index++;
+            }
 ?>
 </tbody>
 </table>
 <?php
+        } else {
+?>
+<h3><?php echo WEENIE_TYPE[$type]; ?> </h3>
+<table class="horizontal-table lookup-table" align="center">
+<thead>
+    <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Code</th>
+    </tr>
+</thead>
+<tbody>
+<?php
+            $index = 0;
+            foreach ($results as $row) {
+                $rowClass = $index % 2 == 1 ? ' class="alt"' : '';
+?>
+    <tr<?php echo $rowClass; ?>>
+        <td><?php echo $row['id']; ?></td>
+        <td><a href="mob.php?id=<?php echo $row['id']; ?>"><?php echo $row['name']; ?></a></td>
+        <td><?php echo $row['code']; ?></td>
+    </tr>
+<?php
+                $index++;
+            }
+?>
+</tbody>
+</table>
+<?php
+        }
+    }
 }
 
 include_once 'footer.php';
